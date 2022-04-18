220b8bc311bd885d5abe2f08db0860342eb64298
e-crede <e-crede@outlook.com>
2022-04-18 15:07:45 +0200

    devise & rails7 bugfixing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :reminders
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
